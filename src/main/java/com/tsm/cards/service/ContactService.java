47e05be7dc1490f9aa64eb798644dc922ba0747e
tomasmaiorino <tomasmaiorino@gmail.com>
2018-06-13 23:20:04 -0300

    add netflix lib and discovery call

--- src/main/java/com/tsm/cards/service/ContactService.java
+++ src/main/java/com/tsm/cards/service/ContactService.java
@@ -3,18 +3,43 @@ package com.tsm.cards.service;
 import com.tsm.cards.resources.ContactResource;
 import io.jsonwebtoken.lang.Assert;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 /**
  * Created by tomas on 6/10/18.
  */
 @Slf4j
+@Service
 public class ContactService {
+
+    @Value("${contact.application.url}")
+    private String contactApplicationUrl;
+
+    @Value("${englishCardClientToken}")
+    private String englishCardClientToken;
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     public boolean sendContactMessage(final ContactResource contact) {
         Assert.notNull(contact, "The contact must not be null!");
         log.info("Sending contact message [{}].");
-        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<ContactResource> exchange =
+                this.restTemplate.exchange(
+                        contactApplicationUrl,
+                        HttpMethod.POST,
+                        null,
+                        new ParameterizedTypeReference<ContactResource>() {
+                        },
+                        englishCardClientToken);
         return  false;
     }
 }
